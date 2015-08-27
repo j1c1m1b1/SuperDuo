40cd77f92fb596a17d7e97f08c0aff43efe9a7cd
jcmb <j1c1m1b1@gmail.com>
2015-08-26 22:39:53 -0500

    Corrected database contract WTF? and changed the build tools platform

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/data/DatabaseContract.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/data/DatabaseContract.java
@@ -10,44 +10,11 @@ import android.provider.BaseColumns;
 public class DatabaseContract
 {
     public static final String SCORES_TABLE = "scores_table";
-    public static final class scores_table implements BaseColumns
-    {
-        //Table data
-        public static final String LEAGUE_COL = "league";
-        public static final String DATE_COL = "date";
-        public static final String TIME_COL = "time";
-        public static final String HOME_COL = "home";
-        public static final String AWAY_COL = "away";
-        public static final String HOME_GOALS_COL = "home_goals";
-        public static final String AWAY_GOALS_COL = "away_goals";
-        public static final String MATCH_ID = "match_id";
-        public static final String MATCH_DAY = "match_day";
-
-        //public static Uri SCORES_CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH)
-                //.build();
-
-        //Types
-        public static final String CONTENT_TYPE =
-                ContentResolver.CURSOR_DIR_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH;
-        public static final String CONTENT_ITEM_TYPE =
-                ContentResolver.CURSOR_ITEM_BASE_TYPE + "/" + CONTENT_AUTHORITY + "/" + PATH;
-
-        public static Uri buildScoreWithLeague()
-        {
-            return BASE_CONTENT_URI.buildUpon().appendPath("league").build();
-        }
-        public static Uri buildScoreWithId()
-        {
-            return BASE_CONTENT_URI.buildUpon().appendPath("id").build();
-        }
-        public static Uri buildScoreWithDate()
-        {
-            return BASE_CONTENT_URI.buildUpon().appendPath("tvDate").build();
-        }
-    }
     //URI data
     public static final String CONTENT_AUTHORITY = "barqsoft.footballscores";
     public static final String PATH = "scores";
+    public static Uri BASE_CONTENT_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
+
     public static final class scores_table implements BaseColumns
     {
         //Table data
@@ -62,7 +29,7 @@ public class DatabaseContract
         public static final String MATCH_DAY = "match_day";
 
         //public static Uri SCORES_CONTENT_URI = BASE_CONTENT_URI.buildUpon().appendPath(PATH)
-                //.build();
+        //.build();
 
         //Types
         public static final String CONTENT_TYPE =
@@ -80,7 +47,7 @@ public class DatabaseContract
         }
         public static Uri buildScoreWithDate()
         {
-            return BASE_CONTENT_URI.buildUpon().appendPath("tvDate").build();
+            return BASE_CONTENT_URI.buildUpon().appendPath("date").build();
         }
-    }    public static Uri BASE_CONTENT_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
+    }
 }
